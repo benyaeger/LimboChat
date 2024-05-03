cebb2a32a8b765bceb77fb9ec881b6b713b70d34
benyaeger <benyaeger13@gmail.com>
2024-05-03 21:29:42 +0300

    TCP connection method implementation i Client.py and Server.py

--- Client.py
+++ Client.py
@@ -2,11 +2,13 @@ from socket import *
 
 serverName = '10.0.0.15'
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+print('connection has been established with {}'.format((serverName, serverPort)))
 message = input('Enter string: ')
 print('Sending message to ({},{})...'.format(serverName, serverPort))
-clientSocket.sendto(message.encode(), (serverName, serverPort))
+clientSocket.send(message.encode())
 print('message sent to ({},{})'.format(serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print('{} responded: {}'.format(serverAddress, modifiedMessage.decode()))
+modifiedMessage = clientSocket.recv(2048)
+print('{} responded: {}'.format((serverName, serverPort), modifiedMessage.decode()))
 clientSocket.close()

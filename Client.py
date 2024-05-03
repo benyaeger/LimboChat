e617be71b04d648ea6614e11deea5418deab2967
benyaeger <benyaeger13@gmail.com>
2024-05-03 19:03:47 +0300

    Print lines added, server host updated

--- Client.py
+++ Client.py
@@ -1,10 +1,11 @@
 from socket import *
 
-serverName = 'hostname'
+serverName = '10.0.0.15'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = input('Enter string: ')
 clientSocket.sendto(message.encode(), (serverName, serverPort))
+print('message sent to ({},{})'.format(serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recv(2048)
 print(modifiedMessage)
 clientSocket.close()

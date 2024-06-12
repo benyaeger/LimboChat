abeb5b5a7cf233ced5d7e8de6104ca1141e77181
benyaeger <benyaeger13@gmail.com>
2024-06-12 12:34:40 +0300

    Added prints to server

--- Server.py
+++ Server.py
@@ -4,11 +4,16 @@ serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
-print('server is up and ready')
-connectionSocket, clientAddr = serverSocket.accept()
-print('connection established with {}'.format(clientAddr))
+print('Server is up and ready to accept connections on port', serverPort)
+
 while True:
-    message = connectionSocket.recv(2048).decode()
-    print('{}: {}'.format(clientAddr, message))
-    response = input('enter:')
-    connectionSocket.send(response.encode())
+    connectionSocket, clientAddr = serverSocket.accept()
+    print('Connection established with {}'.format(clientAddr))
+    while True:
+        message = connectionSocket.recv(2048).decode()
+        if not message:  # Handle client disconnection
+            print('Client {} disconnected'.format(clientAddr))
+            break
+        print('{}: {}'.format(clientAddr, message))
+        response = input('Enter response: ')
+        connectionSocket.send(response.encode())
